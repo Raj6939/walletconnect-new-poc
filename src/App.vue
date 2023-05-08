7ae57c31cefa3d946049a56f94244892ae3e37f7
raj6939 <raj.v.patil108@gmail.com>
2023-05-08 23:24:20 +0530

    add metatags og

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,31 @@
 
 export default {
   name: 'App', 
+  metaInfo: {
+    title: 'Your Page Title',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Your Page Title'
+      },
+      {
+        property: 'og:description',
+        content: 'Description of your webpage'
+      },
+      {
+        property: 'og:image',
+        content: 'https://media.licdn.com/dms/image/C4D03AQGM3Z9dL6ZkoQ/profile-displayphoto-shrink_800_800/0/1655765136300?e=2147483647&v=beta&t=LuvfT4R4SwW7XmMEXo2e9SIrQ5uKlMs_2saK2Gi_iOI'
+      },
+      {
+        property: 'og:url',
+        content: 'URL of your webpage'
+      },
+      {
+        property: 'og:type',
+        content: 'website'
+      }
+    ]
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import {BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueSweetalert2 from 'vue-sweetalert2'
+import VueMeta from 'vue-meta'
 import 'sweetalert2/dist/sweetalert2.min.css';
 Vue.use(BootstrapVue);
 Vue.use(VueSweetalert2);
 Vue.config.productionTip = false
-
+Vue.use(VueMeta)
 new Vue({
   router,
   render: h => h(App),
